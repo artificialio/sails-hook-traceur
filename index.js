18296ac6d8b62bb1f8812f3558e4835c93b053dc
Globegitter <markus.padourek@gmail.com>
2015-01-20 15:19:01 +0000

    Managed to get traceur-hook to work via configure function. First release.

--- index.js
+++ index.js
@@ -22,25 +22,21 @@ module.exports = function(sails) {
      * Initialize the hook
      * @param  {Function} cb Callback for when we're done initializing
      */
-    initialize: function(cb) {
+    configure: function() {
 
       // If the hook has been deactivated, just return
       if (!sails.config[this.configKey].compile) {
         sails.log.verbose("Traceur hook deactivated.");
-        return cb();
+      } else {
+
+        //Load Traceur and override the default require; with async/await on.
+        require('traceur').require.makeDefault(function(filename) {
+          // don't transpile our dependencies, just our app
+          return filename.indexOf('node_modules') === -1;
+        }, { asyncFunctions: true });
+
+        sails.log.verbose("Traceur hook activated. Enjoy ES6/7 power in your Sails app.");
       }
-
-      //Load Traceur and override the default require; with async/await on.
-      require('traceur').require.makeDefault(function(filename) {
-        // don't transpile our dependencies, just our app
-        return filename.indexOf('node_modules') === -1;
-      }, { asyncFunctions: true });
-
-      sails.log.verbose("Traceur activated. Enjoy ES6/7 power in your Sails app.");
-
-      // We're done initializing.
-      return cb();
-
     },
 
   };
